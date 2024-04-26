9e7ce1df52c3fa2ac57f84a8b21a7d528c902c2e
leeleexiaoyao <121509718+leeleexiaoyao@users.noreply.github.com>
2024-04-26 18:30:58 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,7 +25,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://explorechina.info', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'China, Trvale', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
